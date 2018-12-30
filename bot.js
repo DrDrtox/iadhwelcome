c5054c1e8218da0dcb2e32fc03757a3af42fc5cf
iadh123zfest <45603056+iadh123zfest@users.noreply.github.com>
2018-12-30 12:09:00 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,11 @@
-const Discord = require('discord.js'),
+onst Discord = require('discord.js'),
     bot = new Discord.Client({sisableEveryone: true})
     console.log("muuuuuuuuute,");
 bot.on('guildMemberAdd', member => {
 const mohamed= member.guild.channels.get("524624748047499264");
 if(!mohamed) return;
 if(mohamed) {
-setTimeout(() => mohamed.send(`**Welcome, To NIX NetWork **`), 4000)        
+setTimeout(() => mohamed.send(`**Welcome To NIX NetWork**`), 4000)        
 }
 });
 
